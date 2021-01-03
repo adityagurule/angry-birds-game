bd2f67dff3beb61a7625a7acbefcbcc09dbf3099
adityagurule <68185137+adityagurule@users.noreply.github.com>
2021-01-03 10:57:33 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,33 @@
+class Pig extends Base{
+    constructor(x, y) {
+      super(x,y,50,50);
+      this.image= loadImage("images/enemy.png");
+      this.v= 255;
+
+     }
+
+
+       display(){
+        //  console.log (this.body.speed);
+
+         if(this.body.speed>3){
+          World.remove(world,this.body);
+          push ();
+          this.v= this.v-5;
+          tint (255,this.v);
+          image (this.image,this.body.position.x, this.body.position.y,50,50);
+          // score= score+200;
+          pop ();
+         }else {
+          super.display();
+
+         }
+       }
+
+      score(){
+        if(this.v<0 && this.v> -1005){
+          score++;
+        }
+      }
+  }
+  
